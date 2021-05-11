223d3a5c88e1c137a834b369ac39f265e3c3cad2
helipoc <klonja0x@gmail.com>
2021-05-11 00:01:05 +0000

    react tasks app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import React, { useState } from "react"
-import { Input } from "antd";
+import React, { useState, useEffect } from "react"
+import { Input, notification } from "antd";
 import Todos from "./Todos";
 function App() {
   const [todos, setTodo] = useState([])
   const [text, setText] = useState("")
   const handlSub = (e) => {
     if (e.key == "Enter") {
+      if (text == "") {
+        notification["error"]({
+          message: 'Task field is empty ',
+          duration: 2
+        })
+        return;
+      }
       setTodo(todo => [...todo, text])
       setText('')
+      notification["success"]({
+        message: 'Added Task !',
+        duration: 2
+      })
     }
   }
 
   const handlDel = (id) => {
     setTodo(todo => [...todo].filter((t, i) => i != id))
+    notification["info"]({
+      message: 'Task Done !',
+      duration: 2
+    })
+
   }
+  useEffect(() => {
+    let saved = localStorage.getItem('todos')
+    if (saved && saved.length > 0) {
+      setTodo(JSON.parse(saved))
+    }
+  }, [])
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
   return (
     <>
       <center>
-        <div style={{ marginTop: "5em", width: "50%" }}>
+        <div style={{ marginTop: "5em", marginBottom: "3em", width: "50%" }}>
           <Input placeholder="Add Task ... " onKeyDown={handlSub} onChange={e => setText(e.target.value)} value={text} />
         </div>
         <br />
-        <Todos todos={todos} handl={handlDel} />
+        {todos.length > 0 ? <Todos todos={todos} handl={handlDel} /> :
+          <div className="animate__animated animate__zoomIn">
+            <img src="https://digitalsynopsis.com/wp-content/uploads/2016/06/loading-animations-preloader-gifs-ui-ux-effects-10.gif" width="300" height="300" />
+            <br />
+            <b style={{ fontFamily: "monospace" }}>Your Tasks list is empty !</b>
+          </div>
+        }
       </center>
     </>)
 }
--- src/Todos.js
+++ src/Todos.js
@@ -1,16 +1,35 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Card } from "antd"
 
 
 function Todos({ todos, handl }) {
+    const animetype = "animate__fadeInUp"
+    const [animes, setAn] = useState([...todos].map(c => `animate__animated ${animetype}`))
+
+    useEffect(() => {
+        setAn([...todos].map(c => `animate__animated ${animetype}`))
+    }, [todos])
+    const hndl = (i) => {
+        setAn([...todos].map((c, j) => i == j ? "animate__animated animate__fadeOutRight" : `animate__animated ${animetype}`))
+        setTimeout(() => handl(i), 500)
+    }
+
     return (
-        <div style={{ width: "40em  " }}>
+        <div style={{ width: "50%" }}>
             <Card hoverable>
                 <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", font: "25px arial, sans-serif" }}>
-                    {todos.map((c, i) => <p key={i} onClick={() => handl(i)}>{c}</p>)}
+                    {todos.map((c, i) => {
+                        return (
+                            <div key={i} style={{ display: "flex", width: "100%" }} className={animes[i]}>
+                                <p>{c}</p>
+                                <i className="fas fa-check-circle" style={{ marginLeft: "auto", color: "green" }} onClick={() => hndl(i)} />
+                            </div>
+                        )
+
+                    })}
                 </div>
             </Card>
-        </div>
+        </div >
 
     )
 }
